8df6140b95a060394b9f87c0157306a86366bbbb
MohamedAyoubOuerghi <mohamedayoubouerghi@gmail.com>
2022-03-27 22:18:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useEffect, useState } from 'react'
+import xa from 'src\Components\xa.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullname: "mohamed ayoub ouerghi",
+      bio: "chairman in engineers spark ",
+      img:<ayoub/>,
+      profession: "etudiant  ",
+      count:0}
+    }
+
+
+    state={divcontainer:false}
+  render() {
+    const {count}=this.state
+
+
+    var Handlechange = e => {
+      this.setState({divcontainer:!this.state.divcontainer})
+    }
+    const x=this.state.divcontainer;
+
+    return (
+      <div> 
+        <button onClick={Handlechange}>{x?'Hide':'Show'}</button>
+          {x &&(<div>
+              
+          <center>
+          
+          <h1 >My name is {this.state.fullname}</h1>
+          <p>something i want to say {this.state.bio}</p>
+          <p>I want to be {this.state.profession}</p>
+          <p>here is a picture . {this.state.img}</p>
+          <h2>{count}</h2>
+           </center>
+      </div>)}
+      </div>
+    )
+  }
+
+  
+
+  componentDidMount () {
+  this.myInterval = setInterval(()=>{
+    this.setState(prevState => ({
+      count: this.state.count +1
+    }))
+  },1000)
+}
 }
 
 export default App;
---
+++ src/Components/xa.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Image from 'src\ayoub.png'
+
+function ayoub () {
+    return (
+        <div> 
+        
+        <img src={Image}/>
+        </div>
+    )}
+
+export default ayoub 
